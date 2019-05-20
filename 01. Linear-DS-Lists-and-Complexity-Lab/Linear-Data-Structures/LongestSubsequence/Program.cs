1decd23293886f57076c9c19c15ec48b0395b219
delian1986 <delian1914@gmail.com>
2019-05-20 21:57:10 +0300

    linear data structures

---
+++ 01. Linear-DS-Lists-and-Complexity-Lab/Linear-Data-Structures/LongestSubsequence/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LongestSubsequence
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> nums = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToList();
+
+            int maxNum = 0;
+            int maxCount = 0;
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                int currCount = 1;
+                for (int j = i+1; j < nums.Count; j++)
+                {
+                    if (nums[j]==nums[i])
+                    {
+                        currCount++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (currCount>maxCount)
+                {
+                    maxNum = nums[i];
+                    maxCount = currCount;
+                }
+            }
+
+            for (int i = 0; i < maxCount; i++)
+            {
+                Console.Write(maxNum+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 01. Linear-DS-Lists-and-Complexity-Lab/Linear-Data-Structures/RemoveOddOccurences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveOddOccurences
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => int.Parse(x))
+            .ToList();
+
+            Dictionary<int, int> occurrences = new Dictionary<int, int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (!occurrences.ContainsKey(numbers[i]))
+                {
+                    occurrences.Add(numbers[i], 0);
+                }
+
+                occurrences[numbers[i]] += 1;
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (occurrences[numbers[i]] % 2 == 0)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+        }
+    }
+}
