d68a355de120258651040d3f29ee794ab2c36f7c
delian1986 <delian1914@gmail.com>
2019-05-20 20:39:09 +0300

    ArrayList

---
+++ 01. Linear-DS-Lists-and-Complexity-Lab/Linear-Data-Structures/Lists/ArrayList.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+public class ArrayList<T>
+{
+    private T[] data;
+
+    private const int Initial_Capacity = 2;
+
+    public ArrayList()
+    {
+        this.data = new T[Initial_Capacity];
+    }
+
+    public int Count
+    {
+        get;
+
+        private set;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index >= this.Count || index<0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return this.data[index];
+        }
+
+        set
+        {
+            if (index >= this.Count || index<0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            this.data[index] = value;
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (this.data.Length <= this.Count)
+        {
+            this.Resize();
+        }
+        this.data[this.Count++] = item;
+    }
+
+    private void Resize()
+    {
+        T[] newArray = new T[this.Count * 2];
+        Array.Copy(this.data, newArray, this.Count);
+        this.data = newArray;
+    }
+
+    public T RemoveAt(int index)
+    {
+        if (index >= this.Count || index < 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        this.Count--;
+        T item = this.data[index];
+        for (int i = index; i < this.Count; i++)
+        {
+            this.data[i] = this.data[i + 1];
+        }
+
+        if (this.Count <= this.data.Length / 4)
+        {
+            this.Shrink();
+        }
+        return item;
+    }
+
+    private void Shrink()
+    {
+        T[] newArray = new T[this.data.Length / 2];
+        Array.Copy(this.data, newArray, this.Count);
+        this.data = newArray;
+    }
+}
---
+++ 01. Linear-DS-Lists-and-Complexity-Lab/Linear-Data-Structures/Lists/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        ArrayList<int> list = new ArrayList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        list.RemoveAt(2);
+        System.Console.WriteLine();
+
+    }
+}
